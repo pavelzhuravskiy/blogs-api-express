93e37c5bcd1b420f0b8f3b29d19d0f17f3eb126d
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-10 15:25:31 +0200

    Revert "Added testing-router.ts"
    This reverts commit 07be568a64f3ac82010656ac84d2f00727756568.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { blogsRouter } from "./routers/blogs-router";
 import { postsRouter } from "./routers/posts-router";
-import { testingRouter } from "./routers/testing-router";
 
 const app = express();
 const port = 3000;
@@ -10,7 +9,6 @@ app.use(express.json());
 
 app.use("/blogs", blogsRouter);
 app.use("/posts", postsRouter);
-app.use("/testing", testingRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- src/routers/testing-router.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, Router } from "express";
-
-export const testingRouter = Router({});
-
-testingRouter.delete("/all-data", (req: Request, res: Response) => {
-  res.status(204).json([]);
-});
