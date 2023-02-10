07be568a64f3ac82010656ac84d2f00727756568
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-10 15:08:00 +0200

    Added testing-router.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { blogsRouter } from "./routers/blogs-router";
 import { postsRouter } from "./routers/posts-router";
+import { testingRouter } from "./routers/testing-router";
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/blogs", blogsRouter);
 app.use("/posts", postsRouter);
+app.use("/testing", testingRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
