b8e46e9cf6a300494c7fc49f4463e2929000be46
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-13 19:09:41 +0200

    Delete all data fixed

--- src/middlewares/blog-id-check-middleware.ts
+++ src/middlewares/blog-id-check-middleware.ts
@@ -1,11 +1,10 @@
 import { body } from "express-validator";
 import { blogsRepository } from "../repositories/blogs-repository";
 
-export const blogIdCheckMiddleware = body("blogId").custom((value, { req }) => {
+export const blogIdCheckMiddleware = body("blogId").custom((value) => {
   const blogsArray = blogsRepository.findAllBlogs();
-  const blogIdToFind = req.body.blogId;
   const foundMatchId = blogsArray.filter(
-    (id, index) => blogIdToFind === blogsArray[index].id
+    (id, index) => value === blogsArray[index].id
   );
   return foundMatchId.length > 0;
 });
--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -1,7 +1,11 @@
 import { Request, Response, Router } from "express";
+import { blogsRepository } from "../repositories/blogs-repository";
+import { postsRepository } from "../repositories/posts-repository";
 
 export const testingRouter = Router({});
 
 testingRouter.delete("/all-data", (req: Request, res: Response) => {
-  res.status(204).json([]);
+  blogsRepository.findAllBlogs().length = 0;
+  postsRepository.findAllPosts().length = 0;
+  res.sendStatus(204);
 });
