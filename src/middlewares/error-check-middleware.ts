9e43b1d98f0f29b5cf2ddd5bc09527b188ded91a
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-14 17:14:26 +0200

    Bug fixing

--- src/middlewares/error-check-middleware.ts
+++ src/middlewares/error-check-middleware.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { ValidationError, validationResult } from "express-validator";
 
-export const errorCheckMiddleware = async (
+export const errorCheckMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const errorFormatter = async ({ msg, param }: ValidationError) => {
+  const errorFormatter = ({ msg, param }: ValidationError) => {
     return {
       message: msg,
       field: param,
