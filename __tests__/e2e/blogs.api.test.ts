9e5680ef683f46e38bd96a2c44d2ea7e87b53a63
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-27 15:47:53 +0000

    Tests blogs validation refactored

--- __tests__/e2e/blogs.api.test.ts
+++ __tests__/e2e/blogs.api.test.ts
@@ -1,19 +1,33 @@
 import {
   blogCreator,
-  blogReturner, blogsLength,
+  blogReturner,
+  blogsLength,
   blogUpdater,
   eraser,
   eraserWithId,
   firstBlog,
-  firstBlogId, firstPost, firstPostId,
+  firstBlogId,
+  firstPost,
+  firstPostId,
   getter,
-  getterWithId, postCreator, postReturner, postsLength, postUpdater,
-} from "./test-functions"
+  getterWithId,
+  postCreator,
+  postReturner,
+  postsLength,
+  postUpdater,
+} from "./test-functions";
 import {
+  blogNameString,
   blogNewDescriptionString,
   blogNewNameString,
   blogNewWebsiteUrlString,
-  blogsURI, postContentString, postNewTitleString, postShortDescriptionString,
+  blogsURI,
+  longString109,
+  longString17,
+  longString500,
+  postContentString,
+  postNewTitleString,
+  postShortDescriptionString,
   postsURI,
 } from "./test-strings";
 import { emptyOutput } from "./test-objects";
@@ -40,7 +54,6 @@ describe("Blogs CRUD operations", () => {
     expect(response.status).toBe(200);
     expect(response.body).toEqual(emptyOutput);
   });
-
   it("should create new blog", async () => {
     // Trying to create a blog
     const response = await blogCreator();
@@ -51,13 +64,11 @@ describe("Blogs CRUD operations", () => {
     const returnedBlog = await blogReturner();
     expect(blog).toStrictEqual(returnedBlog);
   });
-
   it("should update blog", async () => {
     // Trying to update a blog
     const response = await blogUpdater();
     expect(response.status).toBe(204);
   });
-
   it("should return blog by ID with updated data", async () => {
     // Trying to get blog by ID
     const blogId = await firstBlogId();
@@ -74,7 +85,6 @@ describe("Blogs CRUD operations", () => {
     );
     expect(blog).toStrictEqual(returnedBlog);
   });
-
   it("should delete blog by ID", async () => {
     // Trying to delete a blog
     const blogId = await firstBlogId();
@@ -82,8 +92,8 @@ describe("Blogs CRUD operations", () => {
     expect(response.status).toBe(204);
 
     // Checking result by returning blogs array length
-    const length = await blogsLength()
-    expect(length).toBe(0)
+    const length = await blogsLength();
+    expect(length).toBe(0);
   });
 });
 
@@ -98,13 +108,11 @@ describe("Posts CRUD operations", () => {
     const returnedBlog = await blogReturner();
     expect(blog).toStrictEqual(returnedBlog);
   });
-
   it("should return all posts", async () => {
     const response = await getter(postsURI);
     expect(response.status).toBe(200);
     expect(response.body).toEqual(emptyOutput);
   });
-
   it("should create new post", async () => {
     // Trying to create a post
     const response = await postCreator();
@@ -115,13 +123,11 @@ describe("Posts CRUD operations", () => {
     const returnedPost = await postReturner();
     expect(post).toStrictEqual(returnedPost);
   });
-
   it("should update post", async () => {
     // Trying to update a post
     const response = await postUpdater();
     expect(response.status).toBe(204);
   });
-
   it("should return post by ID with updated data", async () => {
     // Trying to get blog by ID
     const postId = await firstPostId();
@@ -131,14 +137,13 @@ describe("Posts CRUD operations", () => {
     // Checking result by returning updated post
     const post = await firstPost();
     const returnedPost = await postReturner(
-        undefined,
-        postNewTitleString,
-        postShortDescriptionString,
-        postContentString
+      undefined,
+      postNewTitleString,
+      postShortDescriptionString,
+      postContentString
     );
     expect(post).toStrictEqual(returnedPost);
   });
-
   it("should delete post by ID", async () => {
     // Trying to delete a post
     const postId = await firstPostId();
@@ -146,7 +151,110 @@ describe("Posts CRUD operations", () => {
     expect(response.status).toBe(204);
 
     // Checking result by returning blogs array length
-    const length = await postsLength()
-    expect(length).toBe(0)
+    const length = await postsLength();
+    expect(length).toBe(0);
+  });
+});
+
+describe("Blogs validations", () => {
+  it("should NOT create new blog without name", async () => {
+    // Trying to create a blog without name
+    const response = await blogCreator(undefined, null);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog with incorrect name type", async () => {
+    // Trying to create a blog with incorrect name type
+    const response = await blogCreator(undefined, 123);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog with incorrect name length", async () => {
+    // Trying to create a blog with incorrect name length
+    const response = await blogCreator(undefined, longString17);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog without description", async () => {
+    // Trying to create a blog without description
+    const response = await blogCreator(undefined, blogNameString, null);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog with incorrect description type", async () => {
+    // Trying to create a blog with incorrect description type
+    const response = await blogCreator(undefined, blogNameString, 123);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog with incorrect description length", async () => {
+    // Trying to create a blog with incorrect description length
+    const response = await blogCreator(undefined, undefined, longString500);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog without website URL", async () => {
+    // Trying to create a blog without website URL
+    const response = await blogCreator(undefined, undefined, undefined, null);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog with incorrect website URL type", async () => {
+    // Trying to create a blog with incorrect website URL type
+    const response = await blogCreator(undefined, undefined, undefined, 123);
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog with incorrect website URL length", async () => {
+    // Trying to create a blog with incorrect website URL length
+    const response = await blogCreator(
+      undefined,
+      undefined,
+      undefined,
+      longString109
+    );
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
+  });
+  it("should NOT create new blog with incorrect website URL format", async () => {
+    // Trying to create a blog with incorrect website URL format
+    const response = await blogCreator(
+        undefined,
+        undefined,
+        undefined,
+        longString17
+    );
+    expect(response.status).toBe(400);
+
+    // Checking result
+    const length = await blogsLength();
+    expect(length).toBe(0);
   });
 });
--- __tests__/e2e/test-strings.ts
+++ __tests__/e2e/test-strings.ts
@@ -1,27 +1,33 @@
 // Credentials
-export const basicAuthKey = "Authorization"
-export const basicAuthValue = "Basic YWRtaW46cXdlcnR5"
+export const basicAuthKey = "Authorization";
+export const basicAuthValue = "Basic YWRtaW46cXdlcnR5";
 
 // URIs
-export const blogsURI = "/blogs/"
-export const postsURI = "/posts/"
+export const blogsURI = "/blogs/";
+export const postsURI = "/posts/";
 
 // Blog creation strings
-export const blogNameString = "Test Name"
-export const blogDescriptionString = "Test Description"
-export const blogWebsiteUrlString = "https://someurl.com/test"
+export const blogNameString = "Test Name";
+export const blogDescriptionString = "Test Description";
+export const blogWebsiteUrlString = "https://someurl.com/test";
 
 // Blog updating strings
-export const blogNewNameString = "New name"
-export const blogNewDescriptionString = "New Description"
-export const blogNewWebsiteUrlString = "https://someurl.com/new"
+export const blogNewNameString = "New name";
+export const blogNewDescriptionString = "New Description";
+export const blogNewWebsiteUrlString = "https://someurl.com/new";
 
 // Post creation strings
-export const postTitleString = "Test title"
-export const postShortDescriptionString = "Test Short Description"
-export const postContentString = "Test content"
+export const postTitleString = "Test title";
+export const postShortDescriptionString = "Test Short Description";
+export const postContentString = "Test content";
 
 // Post updating strings
-export const postNewTitleString = "Test title"
-export const postNewShortDescriptionString = "Test Short Description"
-export const postNewContentString = "Test content"
+export const postNewTitleString = "Test title";
+export const postNewShortDescriptionString = "Test Short Description";
+export const postNewContentString = "Test content";
+
+// Long strings
+export const longString17 = "Lorem ipsum dolor";
+export const longString109 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore"
+export const longString500 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem";
