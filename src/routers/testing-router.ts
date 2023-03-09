d2b7e115529292781934485dc578fe9b7dc818c0
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-03-09 15:13:02 +0200

    Added deleting all comments in testing router

--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { blogsRepository } from "../repositories/mongodb-blogs-repository";
 import { postsRepository } from "../repositories/mongodb-posts-repository";
 import { usersRepository } from "../repositories/mongodb-users-repository";
+import { commentsRepository } from "../repositories/mongodb-comments-repository";
 
 export const testingRouter = Router({});
 
@@ -9,5 +10,6 @@ testingRouter.delete("/all-data", async (req: Request, res: Response) => {
   await blogsRepository.deleteAll();
   await postsRepository.deleteAll();
   await usersRepository.deleteAll();
+  await commentsRepository.deleteAll();
   res.sendStatus(204);
 });
