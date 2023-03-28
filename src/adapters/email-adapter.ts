6d63a48619c4ed36b162109a2c4e32c0564d94ad
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-03-28 11:52:50 +0300

    SendGrid integration

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -1,33 +1,23 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
+import * as dotenv from "dotenv";
+dotenv.config()
+
 export const emailAdapter = {
   async sendEmail(email: string, subject: string, message: string) {
-    // create reusable transporter object using the default SMTP transport
-
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.zoho.eu',
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
-    const mailOptions = {
-      from: '"Pavel Zhuravskiy" <pavel.zhuravskiy@zohomail.eu>', // sender address
-      to: email, // list of receivers
-      subject: subject, // Subject line
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+    const msg = {
+      to: email, // Change to your recipient
+      from: 'alex.crane.0599@gmail.com', // Change to your verified sender
+      subject: subject,
       html: message,
-    };
-
-    // send mail with defined transport object
-    await transporter.sendMail(mailOptions, function (err, info) {
-      if (err) {
-        console.log(err);
-        return
-      } else {
-        console.log(`E-mail sent: ${info.response}`);
-      }
-    });
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+          console.log('Email sent')
+        })
+        .catch((error) => {
+          console.error(error)
+        })
   },
 }
