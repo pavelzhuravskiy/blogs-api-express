ad8bbea3c30a1f43d7a0a28d961132d21587fe5a
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-03-28 12:00:45 +0300

    Vercel refresh

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -1,23 +1,23 @@
 import sgMail from "@sendgrid/mail";
 import * as dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 
 export const emailAdapter = {
   async sendEmail(email: string, subject: string, message: string) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
     const msg = {
       to: email, // Change to your recipient
-      from: 'alex.crane.0599@gmail.com', // Change to your verified sender
+      from: "alex.crane.0599@gmail.com", // Change to your verified sender
       subject: subject,
       html: message,
-    }
+    };
     sgMail
-        .send(msg)
-        .then(() => {
-          console.log('Email sent')
-        })
-        .catch((error) => {
-          console.error(error)
-        })
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
-}
+};
