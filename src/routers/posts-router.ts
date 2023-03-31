06a450c22a301938498ddee1f4ef77fb77bac1d0
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-03-31 19:43:03 +0300

    Added posts controller

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -1,50 +1,14 @@
-import { Request, Response, Router } from "express";
-import { postsService } from "../domain/posts-service";
+import { Router } from "express";
 import { authBasic } from "../middlewares/auth/auth-basic";
 import { validationPostsInput } from "../middlewares/validations/input/validation-posts-input";
 import { validationErrorCheck } from "../middlewares/validations/_validation-error-check";
-import { ObjectId } from "mongodb";
-import {
-  RequestWithParamsAndQuery,
-  RequestWithQuery,
-} from "../types/request-types";
 import { validationPostsCreation } from "../middlewares/validations/validation-posts-creation";
 import { validationPostsFindByParamId } from "../middlewares/validations/find-by-id/validation-posts-find-by-param-id";
-import { postsQueryRepository } from "../repositories/query-repos/posts-query-repository";
-import { QueryModel } from "../models/global/QueryModel";
 import { ValidationCommentsInput } from "../middlewares/validations/input/validation-comments-input";
-import { commentsQueryRepository } from "../repositories/query-repos/comments-query-repository";
-import { commentsService } from "../domain/comments-service";
 import { authBearer } from "../middlewares/auth/auth-bearer";
-import { StringIdModel } from "../models/global/StringIdModel";
-import {SortOrder} from "mongoose";
+import { postsController } from "../controllers/PostsController";
 
 export const postsRouter = Router({});
-
-postsRouter.get(
-  "/",
-  async (req: RequestWithQuery<QueryModel>, res: Response) => {
-    const foundPosts = await postsQueryRepository.findPosts(
-      Number(req.query.pageNumber) || 1,
-      Number(req.query.pageSize) || 10,
-      req.query.sortBy,
-      req.query.sortDirection as SortOrder
-    );
-    res.json(foundPosts);
-  }
-);
-
-postsRouter.get(
-  "/:id",
-  validationPostsFindByParamId,
-  validationErrorCheck,
-  async (req: Request, res: Response) => {
-    const foundPost = await postsQueryRepository.findPostById(
-      new ObjectId(req.params.id)
-    );
-    res.json(foundPost);
-  }
-);
 
 postsRouter.post(
   "/",
@@ -52,10 +16,16 @@ postsRouter.post(
   validationPostsInput,
   validationPostsCreation,
   validationErrorCheck,
-  async (req: Request, res: Response) => {
-    const newPost = await postsService.createNewPost(req.body);
-    res.status(201).json(newPost);
-  }
+  postsController.createPost
+);
+
+postsRouter.get("/", postsController.getPosts);
+
+postsRouter.get(
+  "/:id",
+  validationPostsFindByParamId,
+  validationErrorCheck,
+  postsController.getPost
 );
 
 postsRouter.put(
@@ -65,17 +35,7 @@ postsRouter.put(
   validationPostsInput,
   validationPostsCreation,
   validationErrorCheck,
-  async (req: Request, res: Response) => {
-    const isUpdated = await postsService.updatePost(
-      new ObjectId(req.params.id),
-      req.body
-    );
-
-    if (isUpdated) {
-      const updatedPost = await postsQueryRepository.findPostById(req.body.id);
-      res.status(204).json(updatedPost);
-    }
-  }
+  postsController.updatePost
 );
 
 postsRouter.delete(
@@ -83,45 +43,12 @@ postsRouter.delete(
   authBasic,
   validationPostsFindByParamId,
   validationErrorCheck,
-  async (req: Request, res: Response) => {
-    const isDeleted = await postsService.deletePost(
-      new ObjectId(req.params.id)
-    );
-    if (isDeleted) {
-      res.sendStatus(204);
-    }
-  }
+  postsController.deletePost
 );
 
-postsRouter.delete("/", authBasic, async (req: Request, res: Response) => {
-  const isDeleted = await postsService.deleteAll();
-  if (isDeleted) {
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404);
-  }
-});
+postsRouter.delete("/", authBasic, postsController.deletePosts);
 
 // +++++ Comments section start +++++
-
-postsRouter.get(
-  "/:id/comments",
-  validationPostsFindByParamId,
-  validationErrorCheck,
-  async (
-    req: RequestWithParamsAndQuery<StringIdModel, QueryModel>,
-    res: Response
-  ) => {
-    const foundComments = await commentsQueryRepository.findComments(
-        Number(req.query.pageNumber) || 1,
-        Number(req.query.pageSize) || 10,
-        req.query.sortBy,
-        req.query.sortDirection as SortOrder,
-      new ObjectId(req.params.id)
-    );
-    res.json(foundComments);
-  }
-);
 
 postsRouter.post(
   "/:id/comments",
@@ -129,14 +56,14 @@ postsRouter.post(
   validationPostsFindByParamId,
   ValidationCommentsInput,
   validationErrorCheck,
-  async (req: Request, res: Response) => {
-    const newComment = await commentsService.createNewCommentByPostId(
-      new ObjectId(req.params.id),
-      req.body,
-      req.user!._id
-    );
-    res.status(201).json(newComment);
-  }
+  postsController.createComment
+);
+
+postsRouter.get(
+  "/:id/comments",
+  validationPostsFindByParamId,
+  validationErrorCheck,
+  postsController.getComments
 );
 
 // ----- Comments section end -----
